f9b1765acfe9aad27867b5c65a28071154bf7ba5
Emanuel Casiano-Diaz <Emanuel.Casiano-Diaz@uvm.edu>
2018-02-26 16:07:08 -0500

    Add files via upload
    Updated Python script based on the new tV_main.jl file

--- script.py
+++ script.py
@@ -3,10 +3,10 @@
 import subprocess
 import numpy as np
 
-program = 'tV_main_neg.jl'
-N = 13          #Max number of particles
-xA = '13'              #Partition size
-xAout = 'l13'
+program = 'tV_main.jl'
+N = 2          #Max number of particles
+xA = '2'              #Partition size
+xAout = 'l2'
 case = 'F'
 siteMax = '1'
 code = 'EOP'
@@ -14,14 +14,22 @@ bc = '--pbc'
 bcOut = 'to be determined'
 uMin = '-1.522878745'  #log_10(0.030000000019365026)
 uMax = '2'   #log_10(100)
+#uMin = '1'  #log_10(0.030000000019365026)
+#uMax = '1'   #log_10(100)
 uNum = '80' #Number of points to be calculated
-alpha = '2'
+alpha = '2'        #Renyi Index
+negLabel = 'yes'   #Type 'yes' if doing negative U run. Otherwise, 'no'.
 
-if program == 'tV_main.jl':
-    extension = '.dat'
+#Paste the right part of the following at the end of subprocess.call() if desired:
+#'--probs'    #Generate file with probabilities respect to subsystem size
+#'--neg'      #Do run using negatives of the specified values
 
-else:
+#Add NEG label to negative U runs
+if negLabel=='yes':
     extension = 'NEG.dat'
+    
+else:
+    extension = '.dat'
 
 for n in range(N,N+1,9873):
 
@@ -36,11 +44,11 @@ for n in range(N,N+1,9873):
     n = str(n)
 
     output = code+bcOut+m+case+n+xAout+'a'+alpha+extension
-
+    
     #Comment/Uncomment desired option:
 
     #1) Log Scale Call
-    subprocess.call(['julia', program,'--u-log', bc, '--u-num', uNum, '--u-min', uMin, '--u-max', uMax,'--site-max', siteMax, m, n, '--ee', xA, '--out', output,'--alpha', alpha])
+    subprocess.call(['julia', program, '--u-log', bc, '--u-num', uNum, '--u-min', uMin, '--u-max', uMax,'--site-max', siteMax, m, n, '--ee', xA, '--out', output,'--alpha', alpha,'--neg'])
 
     #2) Linear Scale Call
     #subprocess.call(['julia', program, bc, '--u-num', uNum, '--u-min', uMin, '--u-max', uMax,'--site-max', siteMax, m, n, '--ee', xA, '--out', output,'--alpha', alpha])
